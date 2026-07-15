001aebf3f879e0b7d51942c2d8b7e1ff24f9ebfc
AksharJain04 <akshar2004@gmail.com>
2026-07-15 16:28:47 +0530

    Endpoint bug fixes

--- DTOs/License/UpdateLicenseStatusDto.cs
+++ DTOs/License/UpdateLicenseStatusDto.cs
@@ -2,6 +2,5 @@ using System.ComponentModel.DataAnnotations;
 namespace LicenseGenerator.Api.Dtos;
 
 public record UpdateLicenseStatusDto(
-    [Required] [RegularExpression("^[A-Z0-9]{5}(-[A-Z0-9]{5}){4}$")] string LicenseKey,
     [Required] string ActivationStatus
 );
